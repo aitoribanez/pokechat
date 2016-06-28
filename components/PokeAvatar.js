335163944af315061fd34ba5632055cbe776f1e0
Aitor Ibañez <freelance@aitoribanez.com>
2016-06-29 00:57:46 +0200

    estilos para la app

--- components/PokeAvatar.js
+++ components/PokeAvatar.js
@@ -8,6 +8,8 @@ import React from 'react';
 export default class PokeAvatar extends React.Component {
 	render() {
 		var url = `http://veekom.com/dex/media/pokmon/main-strites/x-y/${this.props.number}.png`
-		return <img src={url} className="avatar" />
+		return <div className="avatar-container">
+			<img src={url} className="avatar" />
+		</div>
 	}
 }
